7d1f138589004514fec28c44ef640799d7fda09a
Lusomm <lusommeralder@outlook.de>
2022-12-16 08:52:28 -0500

    some changes

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,12 +1,36 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
-const Budget = () => {
-    const { budget } = useContext(AppContext);
 
+const Budget = () => {
+    const { budget, expenses, dispatch } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+   
+    const handleChange = (e) => {
+        if(e.target.value > 20000){
+            alert("The value cannot exceed £20,000");
+        }
+        else if(e.target.value < totalExpenses){
+            alert("The budget is lower than spending");
+        }
+        else{
+            dispatch({
+                type: 'SET_BUDGET',
+                payload: e.target.value,
+            });
+        }
+	};
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>
+                <text>
+                Budget: £
+                    </text>
+                <input value={budget} type='number' onChange={(e) => handleChange(e)}> 
+                </input>
+            </span>
         </div>
     );
 };
